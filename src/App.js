72233791b7e571554ace54cc81fd776c837771f3
soongu <naong526@naver.com>
2023-08-24 09:31:36 +0900

    Cart Modal 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Food/Layout/Header";
 import Meals from "./components/Food/Meals/Meals";
+import Cart from "./components/Food/Cart/Cart";
 
 const App = () => {
 
   return (
     <>
+      <Cart />
       <Header />
       <div id='main'>
         <Meals />
